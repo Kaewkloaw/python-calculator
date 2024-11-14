c8f7fb55d7ff15aa182be3c4b48f888f0a057498
Punchaya Chancharoen <149894554+Kaewkloaw@users.noreply.github.com>
2024-11-14 05:46:04 +0000

    edit test and function

--- calculator.py
+++ calculator.py
@@ -7,21 +7,48 @@ class Calculator:
 
     def multiply(self, a, b):
         result = 0
+        negative = (a > 0) != (b > 0)
+        a, b = abs(a), abs(b)
         for i in range(b):
             result = self.add(result, a)
+        if negative:
+            temp_result = 0
+            for _ in range(abs(result)):
+                temp_result = self.add(temp_result, -1)
+            result = temp_result
+        
         return result
 
     def divide(self, a, b):
         result = 0
-        while a < b:
-            a = self.subtract(a, b)
+        negative = (a > 0) != (b > 0)
+        checkZero = (a == 0)
+        a, b = abs(a), abs(b)
+        if (a == 0) or (b == 0):
+            result = 0
+        else: 
+            while a > b:
+                a = self.subtract(a, b)
+                result += 1
             result += 1
+        if negative:
+            temp_result = 0
+            for _ in range(abs(result)):
+                temp_result = self.add(temp_result, -1)
+            result = temp_result
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        while abs(a) >= abs(b):
+            a = a - b
+        if a < 0 and b > 0:
+            while a < 0:
+                a = a + b
+        elif a > 0 and b < 0:
+            while a > 0:
+                a = a + b
         return a
+
 
 # Example usage:
 if __name__ == "__main__":
@@ -32,3 +59,4 @@ if __name__ == "__main__":
     print("Example: multiplication: ", calc.multiply(2, 3))
     print("Example: division: ", calc.divide(10, 2))
     print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: modulo: ", calc.modulo(3, -2))
